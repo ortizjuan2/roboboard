bad188fa4a855f77b989ebc675183ab8a6a22d25
ortizjuan2 <ortizjuan2@gmail.com>
2017-04-20 20:44:28 -0500

    fix:	Fixing motor rotations
    Fixin motor rotations to allow left and rights turns.

--- src/main/ld293.h
+++ src/main/ld293.h
@@ -5,12 +5,14 @@
 class LD293 {
 
     public:
+        byte enablePin;
         byte in_1A;
         byte in_2A;
         byte in_3A;
         byte in_4A;
 
         LD293(byte input_1A, byte input_2A, byte input_3A, byte input_4A){
+            //enablePin = enable_pin;
             in_1A = input_1A;
             in_2A = input_2A;
             in_3A = input_3A;
@@ -23,13 +25,14 @@ class LD293 {
         }
 
 void go(int speed){
-    // motor 1
+    // motor 1 at the left (front side)
     Serial.print("Go\n");
+    //analogWrite(enablePin, 255);
     digitalWrite(in_1A, LOW); 
     digitalWrite(in_2A, HIGH); 
     //analogWrite(in_2A, speed);
 
-    // motor 2
+    // motor 2 at the left
     digitalWrite(in_3A, HIGH); 
     digitalWrite(in_4A, LOW); 
     
@@ -40,22 +43,23 @@ void stop(){
 
     // motor 1
     Serial.print("Stop\n");
+    //analogWrite(enablePin, 0);
     digitalWrite(in_1A, LOW); 
     digitalWrite(in_2A, LOW);
 
     // motor 2
-    digitalWrite(in_4A, LOW); 
-    digitalWrite(in_3A, LOW);
+    digitalWrite(in_3A, LOW); 
+    digitalWrite(in_4A, LOW);
 }
 
 void left(int speed){
-    // motor 1
+    // motor 1 at the left
     Serial.print("Left\n");
     digitalWrite(in_1A, LOW); 
     digitalWrite(in_2A, HIGH); 
     //analogWrite(in_2A, speed);
 
-    // motor 2
+    // motor 2  at the right
     digitalWrite(in_3A, LOW); 
     digitalWrite(in_4A, HIGH); 
     //   analogWrite(in_4A, speed);
--- src/main/main.ino
+++ src/main/main.ino
@@ -4,12 +4,19 @@
 // Definir Pines para ser usados con el Sonar
 #define trigPin 13
 #define echoPin 12
+
+//LD293 Input pins
+#define enable_pin 11
 #define input_1A 3
 #define input_2A 5
 #define input_3A 6
 #define input_4A 9
 
+
 long distance;
+long dist_final;
+long turn;
+
 Sonar sonar(trigPin, echoPin);
 LD293 motor_driver(input_1A, input_2A, input_3A, input_4A);
 
@@ -19,14 +26,37 @@ void setup() {
 
 void loop() {
   distance = sonar.read();
-  Serial.print(distance);
-    Serial.println(" cm");
+  dist_final = distance;
+  distance = sonar.read();
+  dist_final += distance;
+  dist_final /= 2;
+
+  if (dist_final <=40){
+    turn = random(0,2);
+    if(turn == 0){
+      motor_driver.stop();
+      delay(500);
+      motor_driver.left(255);
+      delay(1000);
+    }else{
+      motor_driver.stop();
+      delay(500);
+      motor_driver.right(255);
+      delay(1000);
+    }
+  }
+  
+  //Serial.print(distance);
+  //  Serial.println(" cm");
     motor_driver.go(255);
-    delay(2000);
+    delay(500);
+    
+    /*
     motor_driver.left(255);
     delay(2000);
     motor_driver.right(255);
     delay(2000);
     motor_driver.stop();
     delay(5000);
+    */
 }
